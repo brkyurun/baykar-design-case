e3e14782a45d673d9ccf76571848683fdcac2f4f
Berkay Urun <berkayurun@outlook.com>
2024-07-13 15:22:14 +0300

    feat: finish Hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -39,8 +39,8 @@ export function Hero({
   heroBlockRows,
 }: HeroProps) {
   return (
-    <section className="bg-white bg-gradient-hero from-white from-45% to-baykar-light-yellow to-45% px-4 py-12 font-roboto desktop:from-baykar-washed-yellow desktop:to-baykar-light-yellow desktop:p-20">
-      <div className="desktop:flex desktop:items-center desktop:justify-between">
+    <section className="desktop:bg-gradient-hero-desktop bg-white bg-gradient-hero from-white from-45% to-baykar-light-yellow to-45% px-4 py-12 font-roboto desktop:from-baykar-washed-yellow desktop:from-[54%] desktop:to-baykar-light-yellow desktop:to-[54%] desktop:p-20">
+      <div className="desktop:flex desktop:items-center desktop:justify-between desktop:gap-20">
         <div className="desktop:flex desktop:flex-col desktop:items-start desktop:gap-8">
           <h2 className="text-center text-[3.5rem] font-bold leading-[3.875rem] desktop:text-left desktop:text-7xl">
             {heroTitle}
@@ -48,7 +48,7 @@ export function Hero({
           <p className="mt-8 text-center text-lg desktop:mt-0 desktop:max-w-[44.625rem] desktop:text-left">
             {heroDescription}
           </p>
-          <div className="mx-auto mt-8 flex items-center justify-center gap-4 px-2 desktop:mt-0">
+          <div className="mx-auto mt-8 flex items-center justify-center gap-4 px-2 desktop:mx-0 desktop:mt-0 desktop:justify-start desktop:px-0">
             {heroButtons.map((button) => (
               <div
                 className="desktop:max-w-[11.25rem]"
@@ -65,7 +65,7 @@ export function Hero({
             ))}
           </div>
         </div>
-        <div className="relative isolate mx-auto mt-8">
+        <div className="relative isolate mx-auto mt-8 desktop:mx-0 desktop:mt-0 desktop:h-[clamp(12.125rem,calc(12.125rem+((1vw-0.64rem)*46.6346)),24.25rem)] desktop:w-[clamp(15.25rem,calc(15.25rem+((1vw-0.64rem)*58.1731)),30.375rem)]">
           <Image
             src={heroImageMobile.src}
             alt={heroImageMobile.alt}
@@ -80,23 +80,23 @@ export function Hero({
             sizes="100vw"
             width={486}
             height={388}
-            className="invisible absolute z-10 hidden h-auto w-full object-cover desktop:visible desktop:ml-auto desktop:block desktop:max-h-[24.25rem] desktop:max-w-[30.375rem]"
+            className="invisible absolute z-10 hidden h-auto w-full object-cover desktop:visible desktop:ml-auto desktop:block"
           />
-          <div className="mx-auto h-[calc(100vw-7.5rem)] w-[calc(100vw-7.5rem)] translate-y-8 rounded-[3.125rem] bg-baykar-orange desktop:max-h-[24.25rem] desktop:max-w-[30.375rem]"></div>
+          <div className="mx-auto h-[calc(100vw-7.5rem)] w-[calc(100vw-7.5rem)] translate-y-8 rounded-[3.125rem] bg-baykar-orange desktop:h-[clamp(11.625rem,calc(11.625rem+((1vw-0.64rem)*44.7115)),23.25rem)] desktop:max-h-[23.25rem] desktop:w-[clamp(11.5rem,calc(11.5rem+((1vw-0.64rem)*44.2308)),23rem)] desktop:max-w-[23rem] desktop:translate-y-11"></div>
         </div>
       </div>
-      <div className="mt-12 flex flex-col items-center justify-center gap-8 py-12">
+      <div className="mt-12 flex flex-col items-center justify-center gap-8 py-12 desktop:mt-40 desktop:flex-row desktop:gap-20 desktop:py-0">
         {heroBlockRows.map((block) => (
           <div
             key={crypto.randomUUID()}
-            className="flex flex-col items-center gap-4 text-center"
+            className="flex flex-col items-center gap-4 text-center desktop:items-start desktop:text-left"
           >
             <Image
               src={block.blockImage.src}
               alt={block.blockImage.alt}
               width={72}
               height={72}
-              className="mx-auto aspect-square h-full max-h-16 w-full max-w-16"
+              className="mx-auto aspect-square h-full max-h-16 w-full max-w-16 desktop:mx-0"
             />
             <h3 className="text-xl font-medium leading-5 text-baykar-black">
               {block.blockTitle}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,40 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['var(--font-roboto)'],
+        roboto: ["var(--font-roboto)"],
       },
       colors: {
-        'baykar-brown': '#78350F',
-        'baykar-black': '#0F172A',
-        'baykar-yellow': '#8C4C00',
-        'baykar-red': '#8C0000',
-        'baykar-light-yellow': '#FEF3C7',
-        'baykar-washed-yellow': '#FEFBED',
-        'baykar-orange': '#FBBF24',
+        "baykar-brown": "#78350F",
+        "baykar-black": "#0F172A",
+        "baykar-yellow": "#8C4C00",
+        "baykar-red": "#8C0000",
+        "baykar-light-yellow": "#FEF3C7",
+        "baykar-washed-yellow": "#FEFBED",
+        "baykar-orange": "#FBBF24",
       },
       screens: {
-        mobile: '392px',
-        desktop: '1024px',
-        'desktop-lg': '1440px',
+        mobile: "392px",
+        desktop: "1024px",
+        "desktop-lg": "1440px",
       },
       container: {
         center: true,
         screens: {
-          DEFAULT: '1440px',
+          DEFAULT: "1440px",
         },
       },
       backgroundImage: {
-        'gradient-hero': 'linear-gradient(135deg, var(--tw-gradient-stops))',
+        "gradient-hero": "linear-gradient(135deg, var(--tw-gradient-stops))",
+        "gradient-hero-desktop":
+          "linear-gradient(150deg, var(--tw-gradient-stops))",
       },
     },
   },
